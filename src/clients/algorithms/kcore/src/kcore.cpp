d43ad33df134b07638acf11abb1b47de236173ad
Eric Hein <ehein6@gatech.edu>
2016-07-20 10:22:51 -0400

    Refactored streaming kcore.

---
+++ src/clients/algorithms/kcore/src/kcore.cpp
@@ -0,0 +1,18 @@
+#include <stdint.h>
+
+#include "stinger_core/stinger.h"
+#include "stinger_core/xmalloc.h"
+#include "stinger_core/stinger_error.h"
+#include "stinger_net/stinger_alg.h"
+#include "stinger_alg/streaming_kcore.h"
+#include "stinger_net/stinger_alg.h"
+#include "stinger_net/stinger_alg_client.h"
+
+using namespace gt::stinger;
+
+int
+main(int argc, char *argv[])
+{
+    StreamingKCore alg;
+    run_alg_as_client(alg);
+}
